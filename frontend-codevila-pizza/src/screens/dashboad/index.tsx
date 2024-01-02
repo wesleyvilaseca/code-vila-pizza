69413126c1bb0a6c060e88779582222a9b9f0bb2
Wesley Vila Seca <wesley.vilaseca@hotmail.com>
2024-01-02 18:52:23 -0300

    feat: cadastro de produtos e listagem de pedidos

--- frontend-codevila-pizza/src/screens/dashboad/index.tsx
+++ frontend-codevila-pizza/src/screens/dashboad/index.tsx
@@ -1,12 +1,144 @@
-import { Header } from "@/components/Header"
+"use client";
+import { Header } from "@/components/Header";
+import styles from './styles.module.scss';
+import { ModalOrder } from '../../components/ModalOrder';
+import { api } from "@/services/apiClient";
+import { useEffect, useState } from "react";
+import { FiRefreshCcw } from 'react-icons/fi';
+import Modal from 'react-modal';
+
+export type OrderItemProps = {
+  id: string;
+  amount: number;
+  order_id: string;
+  product_id: string;
+  product:{
+    id: string;
+    name: string;
+    description: string;
+    price: string;
+    banner: string;
+  }
+  order:{
+    id: string;
+    table: string | number;
+    status: boolean;
+    name: string | null;
+  }
+}
+
+
+type OrderProps = {
+  id: string;
+  table: string | number;
+  status: boolean;
+  draft: boolean;
+  name: string | null;
+}
+
+interface HomeProps{
+  orders: OrderProps[];
+}
 
 export default function DashBoardScreem() {
+  const [orderList, setOrderList] = useState<HomeProps>()
+
+  const [modalItem, setModalItem] = useState<OrderItemProps[]>()
+  const [modalVisible, setModalVisible] = useState(false);
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      setOrderList(await getOrders());
+    };
+
+    fetchOrders();
+
+}, [])
+
+  function handleCloseModal(){
+    setModalVisible(false);
+  }
+
+  async function handleOpenModalView(id: string){
+   
+     const response = await api.get('/order/detail', {
+       params:{
+        order_id: id,
+       } 
+     })
+
+     setModalItem(response.data);
+     setModalVisible(true);
+
+  }
+
+  async function handleFinishItem(id: string){
+    await api.put('/order/finish', {
+      order_id: id,
+    })
+
+    const response = await api.get('/orders');
+
+    setOrderList(response.data);
+    setModalVisible(false);
+  }
+
+
+  async function handleRefreshOrders(){
+
+    const response = await api.get('/orders')
+    setOrderList(response.data);
+
+  }
+
+  Modal.setAppElement('#dashboard');
+
   return (
     <>
     <Header/>
-      <div>
-        DashBoard
-      </div>
+      <main className={styles.container} id="dashboard">
+
+        <div className={styles.containerHeader}>
+          <h1>Últimos pedidos</h1>
+          <button onClick={handleRefreshOrders}>
+            <FiRefreshCcw size={25} color="#3fffa3"/>
+          </button>
+        </div>
+
+        <article className={styles.listOreders}>
+
+          {orderList?.length === 0 && (
+            <span className={styles.emptyList}>
+              Nenhum pedido aberto foi encontrado...
+            </span>
+          )}
+
+          {orderList?.map( item => (
+            <section  key={item.id} className={styles.orderItem}> 
+              <button onClick={ () => handleOpenModalView(item.id) }>
+                <div className={styles.tag}></div>
+                <span>Mesa {item.table}</span>
+              </button>
+            </section>
+          ))}
+                
+        </article>
+
+      </main>
+
+        { modalVisible && (
+          <ModalOrder
+            isOpen={modalVisible}
+            onRequestClose={handleCloseModal}
+            order={modalItem}
+            handleFinishOrder={ handleFinishItem }
+          />
+        )}
     </>
   )
+}
+
+async function getOrders() {
+  const response = await api.get('/orders');    
+  return response.data;
 }
--- frontend-codevila-pizza/src/screens/product/index.tsx
+++ frontend-codevila-pizza/src/screens/product/index.tsx
@@ -1,10 +1,20 @@
 "use client";
 import { Header } from "@/components/Header";
 import styles from './style.module.scss';
-import { FormEvent, useEffect, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { api } from "@/services/apiClient";
 import { useRouter } from 'next/navigation';
+import { FiUpload } from 'react-icons/fi';
+
+type ItemProps = {
+    id: string,
+    name: string
+}
+
+interface CategoryList {
+    categoryList: ItemProps[]
+}
 
 const ProductsScreem = () => {
 
@@ -15,7 +25,7 @@ const ProductsScreem = () => {
     const [avatarUrl, setAvatarUrl] = useState('');
     const [imageAvatar, setImageAvatar] = useState(null);
   
-    const [categories, setCategories] = useState();
+    const [categories, setCategories] = useState<CategoryList>();
     const [categorySelected, setCategorySelected] = useState(0);
 
     useEffect(() => {
@@ -32,15 +42,61 @@ const ProductsScreem = () => {
     async function handleRegister(event: FormEvent) {
         event.preventDefault();
 
-        if(name === '') {
-            toast.error('Nome é um campo obrigatório');
+        try{
+          const data = new FormData();
+    
+          if(name === '' || price === '' || description === '' || imageAvatar === null){
+            toast.error("Preencha todos os campos!");
             return;
-        };
+          }
+    
+          data.append('name', name);
+          data.append('price', price);
+          data.append('description', description);
+          data.append('category_id', categories[categorySelected].id);
+          data.append('file', imageAvatar);
+        
+          await api.post('/product', data);
+    
+          toast.success('Cadastrado com sucesso!')
+    
+        }catch(err){
+          toast.error("Ops erro ao cadastrar!");
+          throw new Error(err);
+
+        } finally {
+            setName('');
+            setPrice('');
+            setDescription('')
+            setImageAvatar(null);
+            setAvatarUrl('');
+        }
     }
 
     function handleChangeCategory(event){
         setCategorySelected(event.target.value)
     }
+
+    function handleFile(e: ChangeEvent<HTMLInputElement>){
+
+        if(!e.target.files){
+          return;
+        }
+    
+        const image = e.target.files[0];
+    
+        if(!image){
+          return;
+        }
+    
+        if(image.type === 'image/jpeg' || image.type === 'image/png'){
+    
+          setImageAvatar(image);
+          setAvatarUrl(URL.createObjectURL(e.target.files[0]))
+    
+        }
+    
+      }
     return(
         <>
             <Header />
@@ -48,6 +104,24 @@ const ProductsScreem = () => {
                 <h1>Cadastrar produto</h1>
 
                 <form onSubmit={handleRegister} className={styles.form}>
+
+                    <label className={styles.labelAvatar}>
+                        <span>
+                            <FiUpload size={15} color="#fff" />
+                        </span>
+
+                        <input type="file" accept="image/png, image/jpeg" onChange={handleFile}/>
+
+                        {avatarUrl && (     
+                            <img 
+                                className={styles.preview}
+                                src={avatarUrl}
+                                alt="Foto do produto" 
+                                width={250}
+                                height={250}
+                            />
+                        )}
+                    </label>
                 
                     <select value={categorySelected} onChange={handleChangeCategory} >
                         {categories?.map( (item, index) => {
